da9f58e0b1b36a23a064a3b5a330d5ce475fda87
Dany <dany@Danys-MacBook-Pro.local>
2020-11-14 18:30:49 -0600

    encode base64 string

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 document.querySelector('form').addEventListener('submit', event => {
   event.preventDefault(); // Stop default browser behavior to submit form to a non-existing backend server
 
-  const input = document.querySelector('input');
-  console.log(input.value);
+  const input = document.querySelector('#message-input');
+  const encrypted = btoa(input.value);
+
+  document.querySelector('#link-input').value = encrypted;
 });
